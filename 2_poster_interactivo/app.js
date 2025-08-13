0e56a76c8ab88cecf99f811b39ec5db8b8b0f3c5
Salome743 <callezapatasalome@gmail.com>
2025-08-13 14:46:44 -0500

    Add files via upload
    caeprtas actualizadas

---
+++ 2_poster_interactivo/app.js
@@ -0,0 +1,28 @@
+const monedas = document.querySelectorAll(".coin")
+const corazones = document.querySelectorAll(".corazon")
+const contadorMonedas = document.querySelector("#contador-monedas")
+const contadorCorazones = document.querySelector("#contador-corazones")
+let totalMonedas = 0 
+let totalCorazones = 0 
+
+console.log(monedas,corazones,contadorMonedas,contadorCorazones)
+
+
+monedas.forEach( function(moneda){
+    /* console.log(moneda) */
+    moneda.addEventListener( 'click', function () {
+        moneda.classList.add( 'saltar' )
+        totalMonedas++
+        contadorMonedas.textContent = totalMonedas
+} )
+ 
+} )
+
+corazones.forEach ( function ( corazon ){
+    
+    corazon.addEventListener( 'click', function () {
+        corazon.classList.add( 'saltar' )
+        totalCorazones++
+       contadorCorazones.textContent = totalCorazones
+} )
+} )
