4a245ddb3a77cb530def3fc7d08e917bbe08709f
Salome743 <callezapatasalome@gmail.com>
2025-08-14 15:33:29 -0500

    audio parte 1

---
+++ 4_audio_video_js/app.js
@@ -0,0 +1,11 @@
+const video = document.querySelector("#video");
+const playvideo = document.querySelector("#play-video");
+const stopvideo = document.querySelector("#stop-video");
+const playstopimg1 = document.querySelector("#play-stop-img1");
+const playstopimg2 = document.querySelector("#play-stop-img2");
+
+console.log(video);
+console.log(playvideo);
+console.log(stopvideo);
+console.log(playstopimg1);
+console.log(playstopimg2);
