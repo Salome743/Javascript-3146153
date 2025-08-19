cc03d7fe9e3d85ae73f171d0cc8ab2eb8bc7c7ff
Salome743 <callezapatasalome@gmail.com>
2025-08-19 15:30:03 -0500

    Add files via upload
    audio parte 2

--- 4_audio_video_js/app.js
+++ 4_audio_video_js/app.js
@@ -3,9 +3,58 @@ const playvideo = document.querySelector("#play-video");
 const stopvideo = document.querySelector("#stop-video");
 const playstopimg1 = document.querySelector("#play-stop-img1");
 const playstopimg2 = document.querySelector("#play-stop-img2");
+const audio1 = document.querySelector("#audio-1");
+const audio2 = document.querySelector("#audio-2");
 
-console.log(video);
+/* console.log(video);
 console.log(playvideo);
 console.log(stopvideo);
 console.log(playstopimg1);
-console.log(playstopimg2);
+console.log(playstopimg2); */
+
+
+
+//Función para reproducir video
+function reproducirVideo () {
+    video.play()
+}
+
+//Evento de click con "addEventlistener" que ejecuta reproducirVideo//...
+
+playvideo.addEventListener( 'click' , reproducirVideo )
+
+//Fución para pausar video
+function pausarVideo () {
+    video.pause()
+
+}
+//Evento de click con "addEventlistener" que ejecuta pausarVideo//...
+
+stopvideo.addEventListener( 'click' , pausarVideo )
+
+
+//toggleAudioUno
+function toggleAudioUno() {
+
+    if ( audio1.paused ) {
+    audio1.play()
+    playstopimg1.textContent = " stop "
+  }else{
+    audio1.pause()
+    playstopimg1.textContent = " play "
+}
+}
+playstopimg1.addEventListener( 'click' , toggleAudioUno )
+
+
+function toggleAudioDos() {
+
+    if ( audio2.paused ) {
+    audio2.play()
+    playstopimg2.textContent = " stop "
+  }else{
+    audio2.pause()
+    playstopimg2.textContent = " play "
+}
+}
+playstopimg2.addEventListener( 'click' , toggleAudioDos )
